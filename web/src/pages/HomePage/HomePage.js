988c52d85c67b0a2c2c9a2178a09687916f02e84
Rich Sturim <rich.sturim@coxautoinc.com>
2020-06-18 15:50:15 -0400

    blogposts cell

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,9 +1,10 @@
 import BlogLayout from 'src/layouts/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
   return (
     <BlogLayout>
-      <p>This is the home page</p>
+      <BlogPostsCell />
     </BlogLayout>
   )
 }
